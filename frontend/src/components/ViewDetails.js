b1a703d1cad2625f56356b208d38fcdda5a608ea
Arjun-Mahanty <arjunmahanty2002@gmail.com>
2023-09-01 15:55:34 +0530

    update view details

--- frontend/src/components/ViewDetails.js
+++ frontend/src/components/ViewDetails.js
@@ -25,47 +25,6 @@ height: 200px;
 const Button = styled.button`
 background-color: white;
 border: none;
-/* &:hover{
-
-  height: 40px;
-  width: 100px;
-  border-radius: 5px;
-  overflow: hidden;
-  position: absolute;
-  &:before{
-    content: "";
-    height:375%;
-    width: 150%;
-    position: absolute;
-    left: -25%;
-    top:-135%;
-    background:conic-gradient(
-      #fd004c,
-      #fe9000,
-      #fff020,
-      #3edf4b,
-      #3363ff,
-      #b102b7,
-      #fd004c
-      );
-      animation:${spin} 1.5s infinite linear ;
-    }
-    &:after{
-      position: absolute;
-    content: "View Details";
-    background-color: black;
-    color: white;
-    
-    height: 80%;
-    width:90%;
-    top:5.5%;
-    left:3.5%;
-    display: grid;
-    place-items:center;
-    border-radius:5px;
-    border: 1px solid black;
-  } */
-/* } */
   `;
 
 export default ViewDetails
